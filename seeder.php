aeb9f429f6351588cbe047709385aff14ae62f66
laravelgr8 <82206712+laravelgr8@users.noreply.github.com>
2022-11-17 19:41:44 +0530

    Create seeder.php

---
+++ seeder.php
@@ -0,0 +1,29 @@
+How to use faker:-
+first create a seeder
+php artisan make:seeder StudenSeeder
+
+use Faker\Factory as Faker;
+public function run()
+{
+	$faker=Faker::create();
+	foreach(range(1,10) as $value){
+		DB::table('students')->insert([
+			"name" => $faker->name(),
+			"city" => $faker->city(),
+			"fee" => $faker->randomFloat(2),
+		]);
+	}
+}
+
+Now go to databaseSeeder.php
+public function run(){
+	$this->call([
+		StudentSeeder::class,
+	]);
+}
+
+Now call seeder
+php artisan db:seed
+
+How to run specific seeder
+php artisan db:seed --class=StudentSeeder
